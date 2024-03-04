385f04f94125bf83d67b04f417fed7f3f728080c
thanielLee <thaniel.lee@student.ateneo.edu>
2024-03-04 18:52:03 +0800

    add: admin pages created

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Recipe, RecipeIngredient, Ingredient
+
+class RecipeIngredientInLine(admin.TabularInline):
+    model = RecipeIngredient
+
+    
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+    inlines = [RecipeIngredientInLine,]
+
+    search_fields = ('name',)
+    list_display = ('name',)
+
+class IngredientAdmin(admin.ModelAdmin):
+    model = Ingredient
+    
+    
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
