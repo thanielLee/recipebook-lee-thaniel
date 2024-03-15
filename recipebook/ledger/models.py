7c2e51fc865aaa9a96ca4c033fda1b5d36644e76
thanielLee <thaniel.lee@student.ateneo.edu>
2024-03-15 16:35:08 +0800

    updated: recipe model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.urls import reverse
+from accounts.models import Profile
+from django.conf import settings
+
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=50)
@@ -12,6 +15,12 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=50)
+    author = models.ForeignKey(
+        Profile,
+        on_delete=models.PROTECT,
+        )
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return '{}'.format(self.name)
